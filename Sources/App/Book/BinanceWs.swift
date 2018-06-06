907edf61ae771dbb697d3790a068a9feffd71bde
Yauheni Yarotski <yauheni_yarotski@me.com>
2018-06-06 14:17:24 +0300

    testing

--- Sources/App/Book/BinanceWs.swift
+++ Sources/App/Book/BinanceWs.swift
@@ -12,43 +12,27 @@ import Console
 class BinanceWs {
   
   static let shared = BinanceWs()
-  let terminal = Terminal()
   
   var ws: WebSocket!
   let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
   
-  init() {
-  }
   
   func start() {
-    terminal.print("start")
+    print("start")
     
     ws = try! HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: worker).wait()
     
     ws.onText { ws, text in
-      //      print("Server echo: \(text)")
-      
+      print(text)
       let ticker = Mapper<Ticker>().map(JSONString: text)!
       
       for priceLevel in ticker.asks {
         if let p = priceLevel.first as? String, let l = priceLevel[1] as? String {
-//          print("asks:", p, l)
-          self.terminal.print("asks:" + " \(p)" + " \(l)")
+          print("asks:", p, l)
         }
       }
-      
-      for priceLevel in ticker.bids {
-        if let p = priceLevel.first as? String, let l = priceLevel[1] as? String {
-//          print("bids:", p, l)
-          self.terminal.print("bids:" + " \(p)" + " \(l)")
-        }
-      }
-      
-      
-      
     }
     
-    // Wait for the Websocket to closre.
     try! ws.onClose.wait()
     
   }
