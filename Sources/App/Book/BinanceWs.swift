3b30e84c9b504c6857001fd2a638f541d0ffeaaf
Yauheni Yarotski <yauheni_yarotski@me.com>
2018-06-06 13:53:53 +0300

    Fixed print

--- Sources/App/Book/BinanceWs.swift
+++ Sources/App/Book/BinanceWs.swift
@@ -7,43 +7,42 @@
 
 import Vapor
 import ObjectMapper
+import Console
 
 class BinanceWs {
   
   static let shared = BinanceWs()
+  let terminal = Terminal()
   
   var ws: WebSocket!
-  let worker = MultiThreadedEventLoopGroup(numThreads: 1)
+  let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
   
   init() {
   }
   
   func start() {
-    print("start")
+    terminal.print("start")
     
-     ws = try! HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: worker).wait()
+    ws = try! HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: worker).wait()
     
     ws.onText { ws, text in
-//      print("Server echo: \(text)")
+      //      print("Server echo: \(text)")
       
       let ticker = Mapper<Ticker>().map(JSONString: text)!
       
       for priceLevel in ticker.asks {
         if let p = priceLevel.first as? String, let l = priceLevel[1] as? String {
-          print("asks:", p, l)
+//          print("asks:", p, l)
+          self.terminal.print("asks:" + " \(p)" + " \(l)")
         }
       }
       
       for priceLevel in ticker.bids {
         if let p = priceLevel.first as? String, let l = priceLevel[1] as? String {
-          print("bids:", p, l)
+//          print("bids:", p, l)
+          self.terminal.print("bids:" + " \(p)" + " \(l)")
         }
       }
-      
-      
-      
-        
-        
       
       
       
