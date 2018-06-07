1b02f6da6326322217204b3158651291b2ac0bb1
Yauheni Yarotski <yauheni_yarotski@me.com>
2018-06-07 15:12:07 +0300

    testing

--- Sources/App/Book/BinanceWs.swift
+++ Sources/App/Book/BinanceWs.swift
@@ -20,28 +20,28 @@ class BinanceWs {
   func start(_ app: Application) {
     print("startdgdf ")
     
-    let done = try! app.client().webSocket("wss://stream.binance.com:9443/ws/btcusdt@depth").flatMap { ws -> Future<Void> in
-      // setup an on text callback that will print the echo
-      ws.onText { ws, text in
-        print("rec: \(text)")
-        // close the websocket connection after we recv the echo
-//        ws.close()
-      }
-      
-      // when the websocket first connects, send message
-//      ws.send("hello, world!")
-      
-      // return a future that will complete when the websocket closes
-      return ws.onClose
-    }
-    
-    print(done) // Future<Void>
-    
-    // wait for the websocket to close
-    try! done.wait()
-      
-//    ws = try! HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: worker).wait()
+//    let done = try! app.client().webSocket("wss://stream.binance.com:9443/ws/btcusdt@depth").flatMap { ws -> Future<Void> in
+//      // setup an on text callback that will print the echo
+//      ws.onText { ws, text in
+//        print("rec: \(text)")
+//        // close the websocket connection after we recv the echo
+////        ws.close()
+//      }
 //
+//      // when the websocket first connects, send message
+////      ws.send("hello, world!")
+//
+//      // return a future that will complete when the websocket closes
+//      return ws.onClose
+//    }
+//
+//    print(done) // Future<Void>
+//
+//    // wait for the websocket to close
+//    try! done.wait()
+    
+//    ws = try! HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: worker).wait()
+//    
 //    ws.onText { ws, text in
 ////      print(text)
 //      let ticker = Mapper<Ticker>().map(JSONString: text)!
