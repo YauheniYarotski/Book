73f9e32ae18f9736a8d254d8d6dc669e4c5cd6a6
Yauheni Yarotski <yauheni_yarotski@me.com>
2018-06-11 20:17:18 +0300

    move wss to backgound

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,11 +1,11 @@
 import Vapor
+import Async
 
 /// Called after your application has initialized.
 
 public func boot(_ app: Application) throws {
   DispatchQueue.global().async {
     BinanceWs.shared.start(app)
-
   }
   
 }
