5562b0c5ecd67c7ec1b88d863484c6c2236ba42e
Yauheni Yarotski <yauheni_yarotski@me.com>
2018-06-12 13:01:19 +0300

    added some more loging

--- Sources/App/Book/BinanceWs.swift
+++ Sources/App/Book/BinanceWs.swift
@@ -13,26 +13,35 @@ class BinanceWs {
   
   static let shared = BinanceWs()
   
-  var ws: WebSocket!
-//  let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+  var ws: WebSocket?
+  //  let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
   func start(_ app: Application) {
-    
+    print("start ws")
     do {
-    ws = try HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: app).wait()
+      ws = try HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: app).wait()
     } catch {
       print("error")
     }
-    ws.onText { ws, text in
-      let ticker = Mapper<Ticker>().map(JSONString: text)!
-
+    ws?.onText { ws, text in
+      guard let ticker = Mapper<Ticker>().map(JSONString: text) else {
+        print(" ticker is nil")
+        return
+        
+      }
+      
       for priceLevel in ticker.asks {
         if let p = priceLevel.first as? String, let l = priceLevel[1] as? String {
           print("asks:", p, l)
         }
       }
     }
-    print("exit")
-    try! ws.onClose.wait()
+    print("before close ws")
+    do {
+      try ws?.onClose.wait()
+    } catch {
+      print("on close error")
+    }
+    
+    print("after close ws")
   }
-  
 }
