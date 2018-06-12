5b501f8c59b92277c43719020f5dab15b9b4de94
Yauheni Yarotski <yauheni_yarotski@me.com>
2018-06-12 15:09:55 +0300

    removed mapper

--- Sources/App/Book/BinanceWs.swift
+++ Sources/App/Book/BinanceWs.swift
@@ -14,26 +14,16 @@ class BinanceWs {
   static let shared = BinanceWs()
   
   var ws: WebSocket?
-  //  let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
   func start(_ app: Application) {
     print("start ws")
     do {
-      ws = try HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: app).wait()
+      ws = try HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: worker).wait()
     } catch {
       print("error")
     }
     ws?.onText { ws, text in
-      guard let ticker = Mapper<Ticker>().map(JSONString: text) else {
-        print(" ticker is nil")
-        return
-        
-      }
-      
-      for priceLevel in ticker.asks {
-        if let p = priceLevel.first as? String, let l = priceLevel[1] as? String {
-          print("asks:", p, l)
-        }
-      }
+      print(text)
     }
     print("before close ws")
     do {
