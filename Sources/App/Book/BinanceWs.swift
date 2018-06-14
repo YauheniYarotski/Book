366b8cede79c6f685487b185f3b8a76f164bbff7
Yauheni Yarotski <yauheni_yarotski@me.com>
2018-06-14 17:51:54 +0300

    removed singletone

--- Sources/App/Book/BinanceWs.swift
+++ Sources/App/Book/BinanceWs.swift
@@ -17,20 +17,7 @@ class BinanceWs {
     let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
   func start(_ app: Application) {
     print("start ws")
-    do {
-      ws = try HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: worker).wait()
-    } catch {
-      print("error")
-    }
-    ws?.onText { ws, text in
-      print(text)
-    }
-    print("before close ws")
-    do {
-      try ws?.onClose.wait()
-    } catch {
-      print("on close error")
-    }
+    
     
     print("after close ws")
   }
--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -4,8 +4,18 @@ import Async
 /// Called after your application has initialized.
 
 public func boot(_ app: Application) throws {
-//  DispatchQueue.global().async {
-    BinanceWs.shared.start(app)
-//  }
+  //  DispatchQueue.global().async {
+  //    BinanceWs.shared.start(app)
+  //  }
+  
+  
+  var ws = try HTTPClient.webSocket(scheme: .wss, hostname: "stream.binance.com", port: 9443, path: "/ws/btcusdt@depth", on: app).wait()
+  
+  ws.onText { ws, text in
+    print(text)
+  }
+  print("before close ws")
+  try ws.onClose.wait()
+  
   
 }
